d22761e6cb375dd124604997ba1fe117194fd506
Ivan Meler <i_ivan@windowslive.com>
2017-01-13 16:35:02 +0100

    Fix app_switch button on edge devices
    Samsung somehow broke drivers for soft keys on edge devices starting from note edge they are called sec_touchscreen in config but exist as touchscreen on running device

--- zero-common.mk
+++ zero-common.mk
@@ -120,6 +120,7 @@ PRODUCT_COPY_FILES += \
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
+    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchscreen.kl \
     $(LOCAL_PATH)/idc/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
     $(LOCAL_PATH)/idc/ft5x06_ts.idc:system/usr/idc/ft5x06_ts.idc \
     $(LOCAL_PATH)/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
